83f076b6bef26a381239b1b1a5db419daa9135bd
Jeramy Sena <jeramy_sena@hotmail.com>
2022-01-26 15:50:31 -0600

    add replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
@@ -15,13 +15,13 @@ const PizzaSchema = new Schema({
   },
   size: {
     type: String,
-    default: "Large",
+    default: 'Large',
   },
   toppings: [],
   comments: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Comment",
+      ref: 'Comment',
     }
   ]
 },
@@ -37,11 +37,11 @@ const PizzaSchema = new Schema({
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.length;
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model("Pizza", PizzaSchema);
+const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
